f54ed087884b2b8949c4dce31036211b39a2c270
indusRealEstate <webtech@indusre.ae>
2024-03-26 12:09:08 +0400

    first commit

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,119 @@
+<footer class="bg-[#001246] ">
+	<div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+		<div class="md:flex md:justify-between">
+			<div class="mb-6 md:mb-0">
+				<span class="text-white font-bold text-3xl">Logo</span>
+
+				<ul class="text-gray-400 font-medium mt-3 space-y-2">
+					<li class="flex items-center gap-1">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="15"
+							height="14"
+							viewBox="0 0 15 14"
+							fill="none"
+						>
+							<path
+								d="M7.49998 1.16669C5.04998 1.16669 2.83331 3.04502 2.83331 5.95002C2.83331 7.80502 4.26248 9.98669 7.11498 12.5009C7.33665 12.6934 7.66915 12.6934 7.89081 12.5009C10.7375 9.98669 12.1666 7.80502 12.1666 5.95002C12.1666 3.04502 9.94998 1.16669 7.49998 1.16669ZM7.49998 7.00002C6.85831 7.00002 6.33331 6.47502 6.33331 5.83335C6.33331 5.19169 6.85831 4.66669 7.49998 4.66669C8.14165 4.66669 8.66665 5.19169 8.66665 5.83335C8.66665 6.47502 8.14165 7.00002 7.49998 7.00002Z"
+								fill="#BDBDBD"
+							/>
+						</svg>
+						<p class="text">Street Name, City Name</p>
+					</li>
+					<li class="flex items-center gap-1">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="15"
+							height="14"
+							viewBox="0 0 15 14"
+							fill="none"
+						>
+							<path
+								d="M4.36167 6.29417C5.20167 7.945 6.555 9.2925 8.20583 10.1383L9.48917 8.855C9.64667 8.6975 9.88 8.645 10.0842 8.715C10.7375 8.93083 11.4433 9.0475 12.1667 9.0475C12.4875 9.0475 12.75 9.31 12.75 9.63083V11.6667C12.75 11.9875 12.4875 12.25 12.1667 12.25C6.68917 12.25 2.25 7.81083 2.25 2.33333C2.25 2.0125 2.5125 1.75 2.83333 1.75H4.875C5.19583 1.75 5.45833 2.0125 5.45833 2.33333C5.45833 3.0625 5.575 3.7625 5.79083 4.41583C5.855 4.62 5.80833 4.8475 5.645 5.01083L4.36167 6.29417Z"
+								fill="#BDBDBD"
+							/>
+						</svg>
+						<a href="tel:9051290512">+9051290512</a>
+					</li>
+					<li class="flex items-center gap-1">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="15"
+							height="14"
+							viewBox="0 0 15 14"
+							fill="none"
+						>
+							<path
+								d="M12.1667 2.33334H2.83335C2.19169 2.33334 1.67252 2.85834 1.67252 3.50001L1.66669 10.5C1.66669 11.1417 2.19169 11.6667 2.83335 11.6667H12.1667C12.8084 11.6667 13.3334 11.1417 13.3334 10.5V3.50001C13.3334 2.85834 12.8084 2.33334 12.1667 2.33334ZM12.1667 4.66668L7.50002 7.58334L2.83335 4.66668V3.50001L7.50002 6.41668L12.1667 3.50001V4.66668Z"
+								fill="#BDBDBD"
+							/>
+						</svg>
+						<a href="mailto:contact@company.com">contact@company.com</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
+				<div>
+					<h2 class="mb-6 text-sm font-semibold uppercase text-white">Legal</h2>
+					<ul class=" text-gray-400 font-medium">
+						<li class="mb-4">
+							<a href="#" class="hover:underline">Privacy Policy</a>
+						</li>
+						<li>
+							<a href="#" class="hover:underline">Terms &amp; Conditions</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<hr class="my-6 sm:mx-auto border-gray-700 lg:my-8" />
+		<div class="sm:flex sm:items-center sm:justify-between">
+			<span class="text-sm sm:text-center text-gray-400"
+				>© 2023 <a href="#" class="hover:underline">Company Name™</a>. All Rights Reserved.
+			</span>
+			<div class="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+				<a href="#" class="text-gray-500 hover:text-white">
+					<svg
+						class="w-4 h-4"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="currentColor"
+						viewBox="0 0 8 19"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<span class="sr-only">Facebook page</span>
+				</a>
+
+				<a href="#" class="text-gray-500 hover:text-white">
+					<svg
+						class="w-4 h-4"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="currentColor"
+						viewBox="0 0 20 17"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<span class="sr-only">Twitter page</span>
+				</a>
+
+				<a href="https://github.com/lyon93/svelte-real-estate-landing" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
+					<svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+						  <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
+					</svg>
+					<span class="sr-only">GitHub account</span>
+				</a>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,87 @@
+<header class="mb-12">
+	<nav class="bg-gray-100 py-3 fixed w-full z-20 top-0 left-0">
+		<div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
+			<a href="/" class="flex items-center">
+				<img src="/images/logo/logo.webp" class="h-full" width="150" height="200" alt="A House" />
+			</a>
+			<div class="flex md:order-2">
+				<button
+					data-collapse-toggle="navbar-sticky"
+					type="button"
+					class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+					aria-controls="navbar-sticky"
+					aria-expanded="false"
+				>
+					<span class="sr-only">Open main menu</span>
+					<svg
+						class="w-5 h-5"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 17 14"
+					>
+						<path
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M1 1h15M1 7h15M1 13h15"
+						/>
+					</svg>
+				</button>
+			</div>
+			<div
+				class="items-center justify-between hidden w-full md:flex md:w-auto md:order-2"
+				id="navbar-sticky"
+			>
+				<ul
+					class="items-center flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0"
+				>
+					<li>
+						<a
+							href="/"
+							class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded md:bg-transparent gold-text md:p-0"
+							aria-current="page">Home</a
+						>
+					</li>
+					<li>
+						<a
+							href="#property"
+							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent hover-gold-text md:p-0"
+							>About Us</a
+						>
+					</li>
+					<li>
+						<a
+							href="#"
+							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent hover-gold-text md:p-0"
+							>Explore Properties</a
+						>
+					</li>
+					<li>
+						<a
+							href="#service"
+							class=" block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent hover-gold-text md:p-0"
+							>Assist Me</a
+						>
+					</li>
+					<li>
+						<button
+							type="button"
+							class="text-green-700 bg-white-700 border-solid border-2 hover:border-green-600 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg md:text-lg px-4 py-2 text-center mr-3 md:mr-0"
+						>
+							<div class="flex flex-row items-center">
+								<img src="/images/whatsapp.png" width="20" height="10" alt="whatsapp" />
+								<span class="pl-2">Whatsapp</span>
+							</div>
+						</button>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
+
+<style lang="scss">
+	@import '$lib/styles/main.scss';
+</style>
