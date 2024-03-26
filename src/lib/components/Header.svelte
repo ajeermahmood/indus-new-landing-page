bb599b316a71239fe653e249e854b8ed8286db84
indusRealEstate <webtech@indusre.ae>
2024-03-26 16:07:59 +0400

    refactor: :zap: refactored new changes.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,14 +1,63 @@
-<header class="mb-12">
-	<nav class="bg-gray-100 py-3 fixed w-full z-20 top-0 left-0">
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		document.addEventListener('scroll', () => {
+			const navbar = document.getElementById('navbar');
+			const whatsappBtn = document.getElementById('whatsapp-btn');
+			const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+			const logoImg = document.getElementById('logo-img');
+			const navbarBtns = document.getElementsByName('navbar-text');
+
+			if (window.scrollY > 0) {
+				navbar.classList.remove('bg-transparent');
+				navbar.classList.add('bg-white');
+				//
+				whatsappBtn.classList.remove('text-white');
+				whatsappBtn.classList.add('text-green-700');
+				//
+				mobileMenuBtn.classList.remove('text-white');
+				mobileMenuBtn.classList.add('text-black');
+				//
+				logoImg.src = '/images/logo/logo.png';
+				//
+				for (let index = 0; index < navbarBtns.length; index++) {
+					navbarBtns[index].classList.add('text-black');
+					navbarBtns[index].classList.remove('text-white');
+				}
+			} else {
+				navbar.classList.remove('bg-white');
+				navbar.classList.add('bg-transparent');
+				//
+				whatsappBtn.classList.remove('text-green-700');
+				whatsappBtn.classList.add('text-white');
+				//
+				mobileMenuBtn.classList.remove('text-black');
+				mobileMenuBtn.classList.add('text-white');
+				//
+				logoImg.src = '/images/logo/logo-white.png';
+				//
+				for (let index = 0; index < navbarBtns.length; index++) {
+					navbarBtns[index].classList.add('text-white');
+					navbarBtns[index].classList.remove('text-black');
+				}
+			}
+		});
+	});
+</script>
+
+<header>
+	<nav id="navbar" class="bg-transparent py-3 fixed w-full z-20 top-0 left-0">
 		<div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
 			<a href="/" class="flex items-center">
-				<img src="/images/logo/logo.webp" class="h-full" width="150" height="200" alt="A House" />
+				<img id="logo-img" src="/images/logo/logo-white.png" class="h-full" width="150" height="200" alt="A House" />
 			</a>
 			<div class="flex md:order-2">
 				<button
+					id="mobile-menu-btn"
 					data-collapse-toggle="navbar-sticky"
 					type="button"
-					class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+					class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white focus:text-black rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
 					aria-controls="navbar-sticky"
 					aria-expanded="false"
 				>
@@ -39,39 +88,49 @@
 				>
 					<li>
 						<a
+							name="navbar-text"
 							href="/"
-							class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded md:bg-transparent gold-text md:p-0"
+							class="text-white block py-2 pl-3 pr-4 bg-purple-700 rounded md:bg-transparent gold-text md:p-0"
 							aria-current="page">Home</a
 						>
 					</li>
 					<li>
 						<a
+							name="navbar-text"
 							href="#property"
-							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent hover-gold-text md:p-0"
+							class="text-white block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent hover-gold-text md:p-0"
 							>About Us</a
 						>
 					</li>
 					<li>
 						<a
-							href="#"
-							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent hover-gold-text md:p-0"
+							name="navbar-text"
+							class="text-white block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent hover-gold-text md:p-0"
 							>Explore Properties</a
 						>
 					</li>
 					<li>
 						<a
+							name="navbar-text"
 							href="#service"
-							class=" block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent hover-gold-text md:p-0"
+							class="text-white block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent hover-gold-text md:p-0"
 							>Assist Me</a
 						>
 					</li>
 					<li>
 						<button
+							id="whatsapp-btn"
 							type="button"
-							class="text-green-700 bg-white-700 border-solid border-2 hover:border-green-600 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg md:text-lg px-4 py-2 text-center mr-3 md:mr-0"
+							class="text-white bg-white-700 border-solid border-2 hover:border-green-600 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg md:text-lg px-4 py-2 text-center mr-3 md:mr-0"
 						>
 							<div class="flex flex-row items-center">
-								<img src="/images/whatsapp.png" width="20" height="10" alt="whatsapp" />
+								<img
+									class="drop-shadow-md"
+									src="/images/whatsapp.png"
+									width="20"
+									height="10"
+									alt="whatsapp"
+								/>
 								<span class="pl-2">Whatsapp</span>
 							</div>
 						</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,73 +1,119 @@
+<script>
+	let propFor = 'buy';
+
+	function changePropFor(param) {
+		propFor = param;
+	}
+</script>
+
 <svelte:head>
 	<title>Real Estate Svelte & TailwindCSS Template: Modern Home & Property Listings</title>
 	<meta name="description" content="Real Estate Template with Svelte and TailwindCss" />
 </svelte:head>
 
-<section id="hero" class=" px-6 pt-12 bg-gray-100">
-	<div class="grid max-w-screen-xl py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-0 lg:pt-16 lg:grid-cols-12">
-		<div class="mr-auto place-self-center lg:col-span-7">
-			<h1
-				class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl"
-			>
-				Service you Deserve,
-				<span class="underline underline-offset-2 decoration-8 gold-underline">
-					People you Trust!</span
-				>
-			</h1>
-			<p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
-				Let's find a home that's perfect for you.
-			</p>
-
-			<form>
-				<label
-					for="default-search"
-					class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label
-				>
-				<div class="relative">
-					<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="28"
-							height="28"
-							viewBox="0 0 28 28"
-							fill="none"
-						>
-							<path
-								d="M13.6668 14.9774C15.5452 14.9774 17.0679 13.4547 17.0679 11.5763C17.0679 9.69799 15.5452 8.17529 13.6668 8.17529C11.7885 8.17529 10.2658 9.69799 10.2658 11.5763C10.2658 13.4547 11.7885 14.9774 13.6668 14.9774Z"
-								stroke="#AEAEAE"
-								stroke-width="2.26736"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-							<path
-								d="M13.667 2.50684C11.2616 2.50684 8.95476 3.46236 7.25391 5.16321C5.55306 6.86406 4.59753 9.1709 4.59753 11.5763C4.59753 13.7212 5.05327 15.1247 6.29805 16.6778L13.667 25.1804L21.0359 16.6778C22.2807 15.1247 22.7364 13.7212 22.7364 11.5763C22.7364 9.1709 21.7809 6.86406 20.08 5.16321C18.3792 3.46236 16.0723 2.50684 13.667 2.50684V2.50684Z"
-								stroke="#AEAEAE"
-								stroke-width="2.26736"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-						</svg>
-					</div>
-					<input
-						type="search"
-						id="default-search"
-						class="block w-full p-4 pl-12 md:p-6 md:pl-12 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-purple-500 focus:border-purple-500"
-						placeholder="Search by Location"
-						required
-					/>
-					<button
-						type="submit"
-						class="text-white absolute right-1.5 bottom-1.5 md:bottom-2.5 bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm md:text-lg px-3 md:px-10 py-3"
-						>Search</button
+<section id="hero" class="bg-gray-100 bg-img-main">
+	<div class="relative isolate pt-14 lg:px-8 backdrop-blur-sm bg-black/30">
+		<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+			<div class="text-center">
+				<h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+					Service you Deserve, People you Trust!
+				</h1>
+				<p class="mt-6 text-lg leading-8 text-white">Let's find a home that's perfect for you.</p>
+				<div class="mt-10 flex items-center justify-center gap-x-6">
+					<a
+						href="#"
+						class="gold-bg rounded-md px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						>Contact Us</a
+					>
+					<a href="#" class="text-sm font-semibold leading-6 text-white"
+						>Learn more <span aria-hidden="true">→</span></a
 					>
 				</div>
-			</form>
+			</div>
 		</div>
-		<div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-			<img src="/images/house1.avif" width="617" height="697" alt="House" />
-		</div>
+		<div
+			class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+			aria-hidden="true"
+		/>
 	</div>
 </section>
+<div class="flex justify-center">
+	<form class="flex flex-row p-5 bg-white relative bottom-14 max-w-5xl rounded-md drop-shadow-lg">
+		<div class="flex flex-row max-w-sm">
+			<button
+				on:click={() => changePropFor('buy')}
+				type="button"
+				class={propFor == 'buy'
+					? 'main-filter-btns-selected w-full border brown-border focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-6 py-2 text-center mr-3 md:mr-1'
+					: 'main-filter-btns-unselected w-full border focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-6 py-2 text-center mr-3 md:mr-1'}
+				>BUY</button
+			>
+			<button
+				on:click={() => changePropFor('rent')}
+				type="button"
+				class={propFor == 'rent'
+					? 'main-filter-btns-selected w-full border brown-border focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-6 py-2 text-center mr-3 md:mr-3'
+					: 'main-filter-btns-unselected w-full border focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-6 py-2 text-center mr-3 md:mr-3'}
+				>RENT</button
+			>
+		</div>
+		<div class="flex flex-row border border-gray-300 rounded-lg bg-gray-50 items-center">
+			<select
+				name=""
+				id=""
+				value="residential"
+				class="min-w-32 block w-full p-4 md:p-3 text-gray-900 rounded-lg bg-gray-50 focus:ring-purple-500 focus:border-purple-500 mr-3"
+			>
+				<option value="residential" label="Residential" />
+				<option value="commercial" label="Commercial" />
+				<option value="off-plan" label="Off-Plan" />
+			</select>
+			<span class="inline-flex items-center min-h-8 w-1 bg-gray-300" />
+			<label
+				for="default-search"
+				class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label
+			>
+			<div class="relative min-w-96">
+				<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="28"
+						height="28"
+						viewBox="0 0 28 28"
+						fill="none"
+					>
+						<path
+							d="M13.6668 14.9774C15.5452 14.9774 17.0679 13.4547 17.0679 11.5763C17.0679 9.69799 15.5452 8.17529 13.6668 8.17529C11.7885 8.17529 10.2658 9.69799 10.2658 11.5763C10.2658 13.4547 11.7885 14.9774 13.6668 14.9774Z"
+							stroke="#AEAEAE"
+							stroke-width="2.26736"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						/>
+						<path
+							d="M13.667 2.50684C11.2616 2.50684 8.95476 3.46236 7.25391 5.16321C5.55306 6.86406 4.59753 9.1709 4.59753 11.5763C4.59753 13.7212 5.05327 15.1247 6.29805 16.6778L13.667 25.1804L21.0359 16.6778C22.2807 15.1247 22.7364 13.7212 22.7364 11.5763C22.7364 9.1709 21.7809 6.86406 20.08 5.16321C18.3792 3.46236 16.0723 2.50684 13.667 2.50684V2.50684Z"
+							stroke="#AEAEAE"
+							stroke-width="2.26736"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						/>
+					</svg>
+				</div>
+				<input
+					type="search"
+					id="default-search"
+					class="block w-full p-4 pl-12 md:p-4 md:pl-12 text-gray-900 rounded-lg bg-gray-50"
+					placeholder="Search by Location"
+					required
+				/>
+				<button
+					type="submit"
+					class="text-black absolute right-1.5 bottom-1.5 md:bottom-1.5 gold-bg focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm md:text-lg px-3 md:px-10 py-2"
+					>Search</button
+				>
+			</div>
+		</div>
+	</form>
+</div>
 
 <section id="service" class="px-6 bg-white py-12">
 	<h2 class="font-bold text-2xl md:text-4xl lg:text-5xl text-center">Service</h2>
